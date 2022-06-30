fde6842f62147036a79a9c985eaa18b9710b7ab2
u3215869@uni.canberra.edu.au <u3215869@uni.canberra.edu.au>
2022-06-30 19:00:31 +1000

    Catalog

---
+++ src2/Services/Catalog/Catalog.api/Data/ICatalogContext.cs
@@ -0,0 +1,12 @@
+﻿using Catalog.api.Entities;
+using MongoDB.Driver;
+
+
+namespace Catalog.api.Data
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
+
